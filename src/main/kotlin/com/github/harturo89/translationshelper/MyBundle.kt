22a3999baf7a36c4f3362da00ed7ebd0cde998e8
GitHub Action <action@github.com>
2025-04-07 11:09:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/harturo89/translationshelper/MyBundle.kt
+++ src/main/kotlin/com/github/harturo89/translationshelper/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.harturo89.translationshelper
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/harturo89/translationshelper/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/harturo89/translationshelper/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.harturo89.translationshelper.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
